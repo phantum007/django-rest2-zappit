2381caa42d1c6942f9709a0deb4c23b98caede44
prabhat shukla <prabhat.shukla30990@gmail.com>
2023-11-04 23:21:38 +0000

    added code

--- zappit-project/posts/views.py
+++ zappit-project/posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Post
+from .serializer import Postserializer
 
 # Create your views here.
+class PostList(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = Postserializer
