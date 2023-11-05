b0bd756fc7b67e146ec00cbff571c1f78f72b409
prabhat shukla <prabhat.shukla30990@gmail.com>
2023-11-05 00:37:43 +0000

    add

--- zappit-project/posts/serializer.py
+++ zappit-project/posts/serializer.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
-from .models import Post
+from .models import Post,Vote
 
 class Postserializer(serializers.ModelSerializer):
+
+    poster = serializers.ReadOnlyField(source='poster.username')
+    poster_id = serializers.ReadOnlyField(source='poster.username')
+
     class Meta :
         model = Post
-        fields = ['id','title', 'url', 'poster', 'created'] 
+        fields = ['id','title', 'url', 'poster','poster_id' ,'created'] 
+
+class Voteserializer(serializers.ModelSerializer):
+
+   
+    class Meta :
+        model = Vote
+        fields = ['id', ] 
--- zappit-project/posts/views.py
+++ zappit-project/posts/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
-from rest_framework import generics
-from .models import Post
-from .serializer import Postserializer
+from rest_framework import generics, permissions
+from .models import Post, Vote
+from .serializer import Postserializer, Voteserializer
 
 # Create your views here.
 class PostList(generics.ListAPIView):
     queryset = Post.objects.all()
     serializer_class = Postserializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(poster=self.request.user)
+
+
+# Create your views here.
+class VoteCreate(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = Voteserializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        post = Post.objects.get(pk=self.kwargs['pk'])
+        return Vote.objects.filter(voter=user, post=post)
+
+    def perform_create(self, serializer):
+        serializer.save(voter=self.request.user, post= Post.objects.get(pk=self.kwargs('pk')))
